76cc6f7ee6737349d3fa5fa5505c536e7dd30ba7
DeepikaDokuparti <s224190699@deakin.edu.au>
2024-04-02 20:05:02 +1100

    Second Commit

---
+++ task2_2P/src/main/java/sit707_week2/Main_2.java
@@ -0,0 +1,8 @@
+package sit707_week2;
+
+public class Main_2 {
+	public static void main( String[] args )
+	{
+		SeleniumOperations2.pinterest_signup_page("https://www.pinterest.com.au/");
+    }
+}
